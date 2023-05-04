605e4864ebe25eef21b2de01bb65ef5a391ce2cf
Kristiyan17 <kristiyan@boxymedia.com>
2023-05-04 10:31:23 +0300

    Updating the home page so its in the package

--- apps/Betfinal/pages/HomePage/HomePage.tsx
+++ apps/Betfinal/pages/HomePage/HomePage.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-
+import { Home } from "ui";
 export default function HomePage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          Welcome to Betfinal
-        </h1>
-        <p className="text-lg text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-      </div>
-    </div>
-  );
+  return <Home brand="Betfinal" />;
 }
